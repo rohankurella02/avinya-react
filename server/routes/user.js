7896b87c20873c0b7a3856d2ac73aa21b2325222
rohankurella02 <rohankurella02@gmail.com>
2022-10-23 11:21:26 +0530

    chat backend

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const asyncHandler = require("express-async-handler");
 const User = require("../../models/User");
 
-userRouter = express.Router();
+const userRouter = express.Router();
 
 userRouter.post(
   "/create-user",
